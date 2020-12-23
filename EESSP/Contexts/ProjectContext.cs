b139d113d03ff69de379be1d6b9d4f8a24718128
Lucian Gaina <gluci@DESKTOP-21H10T6>
2020-12-23 22:11:34 +0200

    Add project files.

---
+++ EESSP/Contexts/ProjectContext.cs
@@ -0,0 +1,33 @@
+﻿using EESSP.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EESSP.Contexts
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext()
+        {
+        }
+
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        { }
+
+        public virtual DbSet<Pacient> Pacient { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-21H10T6\\SQLEXPRESS;Database=EESSP;Trusted_Connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EESSP/Form1.cs
@@ -0,0 +1,30 @@
+﻿using EESSP.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EESSP
+{
+    public partial class Form1 : Form
+    {
+        private ProjectContext _dbContext;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void onLoad(object sender, EventArgs e)
+        {
+            _dbContext = new ProjectContext();
+
+            var pacienti = await _dbContext.Pacient.ToListAsync();
+        }
+    }
+}
